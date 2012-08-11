bd1e09e2b019b539d3708667e92fcd80758a6c63
Admin <Admin@Admin-PC>
2012-08-12 00:06:16 +0530

    Network completed. Parser completed.

--- src/userclasses/StateMachine.java
+++ src/userclasses/StateMachine.java
@@ -4,19 +4,24 @@
 
 package userclasses;
 
+import com.codename1.components.Progress;
 import com.codename1.io.ConnectionRequest;
 import com.codename1.io.JSONParser;
 import com.codename1.io.NetworkEvent;
 import com.codename1.io.NetworkManager;
+import com.codename1.io.Storage;
 import com.codename1.processing.Result;
 import generated.StateMachineBase;
 import com.codename1.ui.*; 
 import com.codename1.ui.events.*;
 import com.codename1.ui.list.DefaultListModel;
+import com.codename1.ui.list.ListModel;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Vector;
 
@@ -25,6 +30,7 @@ import java.util.Vector;
  * @author Your name here
  */
 public class StateMachine extends StateMachineBase {
+    String response = "";
     public StateMachine(String resFile) {
         super(resFile);
         // do not modify, write code in initVars and initialize class members there,
@@ -52,7 +58,7 @@ public class StateMachine extends StateMachineBase {
 
                int chr;
                StringBuffer sb = new StringBuffer();
-               String response = "";
+              
              protected void readResponse(InputStream input) throws IOException {
                   //do something with input stream
                  while ((chr = input.read()) != -1){
@@ -65,15 +71,44 @@ public class StateMachine extends StateMachineBase {
                   
              }
           };
-          
-             Vector modelData = new Vector();
+             Hashtable hashData;
+             Progress loading = new Progress("Downloading..", request);
+             loading.setAutoShow(true);
+             loading.setDisposeOnCompletion(true);
              request.setUrl("http://173.201.20.182:2050/customers.json");
              request.setPost(false);
-             networkManager.addToQueue(request);
+             networkManager.addToQueueAndWait(request); 
+             
+             InputStream is = new ByteArrayInputStream(response.getBytes());
+             JSONParser parser = new JSONParser();
+             hashData = parser.parse(new InputStreamReader(is));
+             System.out.println("Keys:" + hashData);
+             System.out.println("Values:" + hashData.get("root"));
+             //String[] modelData1 = (String[])hashData.get("root");
+             System.out.println("Convert to string array:");
+             Vector modelData = (Vector)hashData.get("root");
+            // modelData.addElement(modelData);
+             Storage.getInstance().writeObject("custStore", modelData);
+             
+             
+             modelData = (Vector)Storage.getInstance().readObject("custStore");
+             System.out.println(modelData);
+             System.out.println(modelData);
+             Vector testData = new Vector();
+             Hashtable testdata = new Hashtable();
+             testdata.put("name", "Rahul");
+             testdata.put("address", "test");
+             testData.addElement(testdata);
              cmp.setModel(new DefaultListModel(modelData));
+            // System.out.println("testdata" + testData);
+            // cmp.setModel(new DefaultListModel(testData));
         }
         catch (Exception e) {
-           
+            
+           Dialog.show("Connection Error", "Unable to connect", "Continue", null);
+           Vector modelData = new Vector();
+           modelData.addElement("No data");
+           cmp.setModel(new DefaultListModel(modelData));
         }
         
         return true;
